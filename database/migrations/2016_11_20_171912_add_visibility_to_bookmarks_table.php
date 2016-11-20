c345cc040614859df43882afd7242248c1c89267
Igors Krasjukovs <tahq69@gmail.com>
2016-11-20 17:23:16 +0200

    visibility migration

---
+++ database/migrations/2016_11_20_171912_add_visibility_to_bookmarks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class AddVisibilityToBookmarksTable
+ */
+class AddVisibilityToBookmarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bookmarks', function (Blueprint $table) {
+            $table->boolean('visible')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bookmarks', function (Blueprint $table) {
+            $table->dropColumn('visible');
+        });
+    }
+}
